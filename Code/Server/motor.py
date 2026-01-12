66e029ae57b3651bec348157b4206a3834a960ea
Davidbuks <61031416+redcartel243@users.noreply.github.com>
2026-01-12 20:16:33 +0100

    Invert motor control signals in set_motor_model method

--- Code/Server/motor.py
+++ Code/Server/motor.py
@@ -65,10 +65,10 @@ class Ordinary_Car:
             self.pwm.set_motor_pwm(5,4095)
     def set_motor_model(self, duty1, duty2, duty3, duty4):
         duty1,duty2,duty3,duty4=self.duty_range(duty1,duty2,duty3,duty4)
-        self.left_upper_wheel(duty1)
-        self.left_lower_wheel(duty2)
-        self.right_upper_wheel(duty3)
-        self.right_lower_wheel(duty4)
+        self.left_upper_wheel(-duty1)
+        self.left_lower_wheel(-duty2)
+        self.right_upper_wheel(-duty3)
+        self.right_lower_wheel(-duty4)
 
     def close(self):
         self.set_motor_model(0,0,0,0)
